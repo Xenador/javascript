2139a0cf666382f46c2763eb56d0198c53e7a041
Ram Arora <145109183+Xenador@users.noreply.github.com>
2024-11-12 19:56:06 +0000

    some important comparisons

---
+++ importantops.js
@@ -0,0 +1,14 @@
+let a = 2+2+"2"
+console.log(a)
+console.log(+true)
+console.log(+"")
+
+//________________some impotant comparisons___________________
+//comparison and equality atr 2 different operators in java script comparison coverts the null to 0 and equality dont 
+console.log(null==0)
+console.log(null>=0)
+console.log(null>0)
+//________________undefined is a special type so for all the comparisons it will give false
+console.log(undefined==0)
+console.log(undefined>=0)
+console.log(undefined>0)
